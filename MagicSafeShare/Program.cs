b9c4992bda3447937bd01265176567737373ae61
柒梦染星河 <xi.xi.li@outlook.com>
2025-03-21 20:24:10 +0800

    高亮显示分类

--- MagicSafeShare/Program.cs
+++ MagicSafeShare/Program.cs
@@ -58,18 +58,18 @@ class Program
         }
         else if (command == "2")
         {
-            // 精细分类映射（包含中文注释）
-            var categories = new Dictionary<string, (string[], string[])>
+            // 精细分类映射（包含中文注释和颜色）
+            var categories = new Dictionary<string, (string[], string[], ConsoleColor)>
             {
-                { "基本相机信息", (new string[] { "Make", "Model", "Software", "SensingMethod" }, new string[] { "制造商", "型号", "软件", "感应方法" }) },
-                { "图像尺寸与分辨率", (new string[] { "ImageLength", "ImageWidth", "PixelYDimension", "PixelXDimension", "YResolution", "XResolution", "ResolutionUnit" }, new string[] { "图像长度", "图像宽度", "像素高度", "像素宽度", "垂直分辨率", "水平分辨率", "分辨率单位" }) },
-                { "时间信息", (new string[] { "DateTime", "DateTimeOriginal", "DateTimeDigitized", "SubsecTimeOriginal", "SubsecTime", "SubsecTimeDigitized", "OffsetTime", "OffsetTimeOriginal", "GPSDateStamp" }, new string[] { "日期时间", "原始日期时间", "数字化日期时间", "原始子秒时间", "子秒时间", "数字化子秒时间", "偏移时间", "原始偏移时间", "GPS日期戳" }) },
-                { "曝光与镜头信息", (new string[] { "ApertureValue", "ExposureBiasValue", "ExposureProgram", "ExposureMode", "ExposureTime", "Flash", "FNumber", "ShutterSpeedValue", "MeteringMode", "FocalLength", "FocalLengthIn35mmFilm", "ISOSpeed", "ISOSpeedRatings", "SensitivityType", "BrightnessValue", "WhiteBalance", "LightSource" }, new string[] { "光圈值", "曝光偏差值", "曝光程序", "曝光模式", "曝光时间", "闪光灯", "光圈", "快门速度值", "测光模式", "焦距", "35mm等效焦距", "ISO速度", "ISO速度等级", "感光度类型", "亮度值", "白平衡", "光源" }) },
-                { "场景与特效", (new string[] { "SceneType", "SceneCaptureType", "FlashpixVersion", "ComponentsConfiguration" }, new string[] { "场景类型", "场景捕捉类型", "Flashpix版本", "组件配置" }) },
-                { "GPS位置信息", (new string[] { "GPSLatitude", "GPSLongitude", "GPSAltitude", "GPSLatitudeRef", "GPSLongitudeRef", "GPSSpeed", "GPSSpeedRef", "GPSAltitudeRef", "GPSTimestamp" }, new string[] { "GPS纬度", "GPS经度", "GPS高度", "GPS纬度参考", "GPS经度参考", "GPS速度", "GPS速度参考", "GPS高度参考", "GPS时间戳" }) },
-                { "厂商及其他信息", (new string[] { "39321", "34970", "34974", "42593", "34973", "40965", "34975", "34967", "34965" }, new string[] { "厂商特定信息1", "厂商特定信息2", "厂商特定信息3", "厂商特定信息4", "厂商特定信息5", "厂商特定信息6", "厂商特定信息7", "厂商特定信息8", "厂商特定信息9" }) },
-                { "图像其他属性", (new string[] { "Orientation", "YCbCrPositioning", "ColorSpace" }, new string[] { "方向", "YCbCr定位", "色彩空间" }) },
-                { "文件信息", (new string[] { "FileName", "FileSize", "FileType" }, new string[] { "文件名", "文件大小", "文件类型" }) },
+                { "基本相机信息", (new string[] { "Make", "Model", "Software", "SensingMethod" }, new string[] { "制造商", "型号", "软件", "感应方法" }, ConsoleColor.Cyan) },
+                { "图像尺寸与分辨率", (new string[] { "ImageLength", "ImageWidth", "PixelYDimension", "PixelXDimension", "YResolution", "XResolution", "ResolutionUnit" }, new string[] { "图像长度", "图像宽度", "像素高度", "像素宽度", "垂直分辨率", "水平分辨率", "分辨率单位" }, ConsoleColor.Magenta) },
+                { "时间信息", (new string[] { "DateTime", "DateTimeOriginal", "DateTimeDigitized", "SubsecTimeOriginal", "SubsecTime", "SubsecTimeDigitized", "OffsetTime", "OffsetTimeOriginal", "GPSDateStamp" }, new string[] { "日期时间", "原始日期时间", "数字化日期时间", "原始子秒时间", "子秒时间", "数字化子秒时间", "偏移时间", "原始偏移时间", "GPS日期戳" }, ConsoleColor.Yellow) },
+                { "曝光与镜头信息", (new string[] { "ApertureValue", "ExposureBiasValue", "ExposureProgram", "ExposureMode", "ExposureTime", "Flash", "FNumber", "ShutterSpeedValue", "MeteringMode", "FocalLength", "FocalLengthIn35mmFilm", "ISOSpeed", "ISOSpeedRatings", "SensitivityType", "BrightnessValue", "WhiteBalance", "LightSource" }, new string[] { "光圈值", "曝光偏差值", "曝光程序", "曝光模式", "曝光时间", "闪光灯", "光圈", "快门速度值", "测光模式", "焦距", "35mm等效焦距", "ISO速度", "ISO速度等级", "感光度类型", "亮度值", "白平衡", "光源" }, ConsoleColor.Green) },
+                { "场景与特效", (new string[] { "SceneType", "SceneCaptureType", "FlashpixVersion", "ComponentsConfiguration" }, new string[] { "场景类型", "场景捕捉类型", "Flashpix版本", "组件配置" }, ConsoleColor.DarkCyan) },
+                { "GPS位置信息", (new string[] { "GPSLatitude", "GPSLongitude", "GPSAltitude", "GPSLatitudeRef", "GPSLongitudeRef", "GPSSpeed", "GPSSpeedRef", "GPSAltitudeRef", "GPSTimestamp" }, new string[] { "GPS纬度", "GPS经度", "GPS高度", "GPS纬度参考", "GPS经度参考", "GPS速度", "GPS速度参考", "GPS高度参考", "GPS时间戳" }, ConsoleColor.DarkMagenta) },
+                { "厂商及其他信息", (new string[] { "39321", "34970", "34974", "42593", "34973", "40965", "34975", "34967", "34965" }, new string[] { "厂商特定信息1", "厂商特定信息2", "厂商特定信息3", "厂商特定信息4", "厂商特定信息5", "厂商特定信息6", "厂商特定信息7", "厂商特定信息8", "厂商特定信息9" }, ConsoleColor.DarkYellow) },
+                { "图像其他属性", (new string[] { "Orientation", "YCbCrPositioning", "ColorSpace" }, new string[] { "方向", "YCbCr定位", "色彩空间" }, ConsoleColor.DarkGreen) },
+                 { "文件信息", (new string[] { "FileName", "FileSize", "FileType" }, new string[] { "文件名", "文件大小", "文件类型" }, ConsoleColor.Blue) },
             };
 
             foreach (var filePath in imageFiles)
@@ -93,26 +93,27 @@ class Program
                     {
                         Console.WriteLine($"\n【{category.Key}】");
                         bool found = false;
+                        SetColor(category.Value.Item3); // 设置分类颜色
                         for (int i = 0; i < category.Value.Item1.Length; i++)
                         {
                             string key = category.Value.Item1[i];
                             string displayName = category.Value.Item2[i]; // 获取中文名称
-                            if (metadata.TryGetValue(key, out string value))
+                            // 优先匹配完整键名（例如 "EXIF:Make"），然后匹配不带前缀的键名（例如 "Make"）
+                            if (metadata.TryGetValue("EXIF:" + key, out string value) || metadata.TryGetValue(key, out value))
                             {
-                                SetColor(ConsoleColor.Green); // 设置为绿色
-                                // 大数据项只显示长度
                                 if (key == "XMP" || key.StartsWith("Profile:") || key.Contains("MakerNote"))
-                                    Console.WriteLine($"  {displayName}: (数据, 长度: {value.Length})");
+                                    Console.WriteLine($"  {key} ({displayName}): (数据, 长度: {value.Length})");
                                 else
-                                    Console.WriteLine($"  {displayName}: {value}");
-                                ResetColor();
+                                    Console.WriteLine($"  {key} ({displayName}): {value}");
                                 found = true;
-                                remaining.Remove(key);
+                                remaining.Remove("EXIF:" + key); // 移除已匹配的键
+                                remaining.Remove(key);     // 移除不带前缀的键
                             }
                         }
+                        ResetColor();
                         if (!found)
                         {
-                            SetColor(ConsoleColor.DarkGray); // 设置为深灰色
+                            SetColor(ConsoleColor.DarkGray);
                             Console.WriteLine("  无相关数据");
                             ResetColor();
                         }
@@ -121,19 +122,19 @@ class Program
                     Console.WriteLine("\n【未分类元数据】");
                     if (remaining.Count > 0)
                     {
+                        SetColor(ConsoleColor.Red);
                         foreach (var kvp in remaining)
                         {
-                            SetColor(ConsoleColor.Red); // 设置为红色
                             if (kvp.Key == "XMP" || kvp.Key.StartsWith("Profile:"))
                                 Console.WriteLine($"  {kvp.Key}: (数据, 长度: {kvp.Value.Length})");
                             else
                                 Console.WriteLine($"  {kvp.Key}: {kvp.Value}");
-                            ResetColor();
                         }
+                        ResetColor();
                     }
                     else
                     {
-                        SetColor(ConsoleColor.DarkGray); // 设置为深灰色
+                        SetColor(ConsoleColor.DarkGray);
                         Console.WriteLine("  无");
                         ResetColor();
                     }
